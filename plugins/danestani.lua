b01c9111e37cc6037275408c1e186674358be09f
sam <samijoniramezani12@gmail.com>
2016-02-18 21:31:29 -0800

    Delete danestani.lua

--- plugins/danestani.lua
+++
@@ -1,3 +0,0 @@
-local database= 'http://umbrella.shayan-soft.ir/txt/' local function run(msg) local res = http.request(database.."danestani.db") local danestani = res:split(",") return danestani[math.random(#danestani)] end
-
-return { description = "Knowing Book", usage= "!danestani : send knowing texts", patterns = {"^[/!]danestani$"}, run = run } S
